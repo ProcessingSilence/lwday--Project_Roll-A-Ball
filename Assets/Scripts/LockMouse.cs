34d65bd742e9e99b25cb05dafc41010bda5eba46
ProcessingSilence <47760744+ProcessingSilence@users.noreply.github.com>
2021-01-28 17:48:15 -0500

    currernt

--- Assets/Scripts/LockMouse.cs
+++ Assets/Scripts/LockMouse.cs
@@ -3,6 +3,7 @@
 public class LockMouse : MonoBehaviour
 {
     public GameObject pauseImage;
+    public MouseOrbitImproved mouseOrbitScript;
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
@@ -18,14 +19,23 @@ public class LockMouse : MonoBehaviour
 
         //Escape input unlocks the Cursor
         if (Input.GetKey(KeyCode.Escape))
+        {
             Cursor.lockState = CursorLockMode.None;
+            mouseOrbitScript.enabled = false;
+        }
 
         // Make absolute sure that the mouse doesn't lock by unlocking the mouse while the pauseImage background is active.
         if (pauseImage.active)
+        {
             Cursor.lockState = CursorLockMode.None;
+            mouseOrbitScript.enabled = false;
+        }
 
         // Clicking locks the mouse again
         if (Input.GetMouseButtonDown(0) && !pauseImage.active)
+        {
             Cursor.lockState = CursorLockMode.Locked;
+            mouseOrbitScript.enabled = true;
+        }
     }
 }
